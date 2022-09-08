d3d3a53f9da0a86caa538a1b60b39937dd9773c8
Ross <rosswilliamsproject@gmail.com>
2022-09-08 20:33:42 +0100

    v6.2 - added footer details

--- src/Components/FooterComponents/Footer.js
+++ src/Components/FooterComponents/Footer.js
@@ -8,45 +8,42 @@ const Footer = () => {
             <div className="footer-container">
                 <div className="left">
                     <div className="email">
-                            <h4 href='rosswilliamsrw11@gmail.com'>
-                                <FaMailBulk size={20} style={{
-                                    color: "#fff", marginRight: "2rem"
-                                }} />
-                                Email
-                            </h4>
+                        <h4 href='rosswilliamsrw11@gmail.com'>
+                            <FaMailBulk size={20} style={{
+                                color: "#fff", marginRight: "2rem"
+                            }} />
+                            rosswilliamsrw11@gmail.com
+                        </h4>
                     </div>
+                    <br></br>
+
                     <div className="GitHub">
-                            <h4 href='https://github.com/RossWilliamsProject'>
-                                <FaGithub size={20} style={{
-                                    color: "#fff", marginRight: "2rem"
-                                }} />
-                                GitHub
-                            </h4>
+                        <h4 href='https://github.com/RossWilliamsProject'>
+                            <FaGithub size={20} style={{
+                                color: "#fff", marginRight: "2rem"
+                            }} />
+                            https://github.com/RossWilliamsProject
+                        </h4>
                     </div>
+                    <br></br>
                     <div className="LinkedIn">
-                            <h4 href='https://www.linkedin.com/in/ross-williams-3a433b213/'>
-                                <FaLinkedin size={20} style={{
-                                    color: "#fff", marginRight: "2rem"
-                                }} />
-                                LinkedIn
-                            </h4>
+                        <h4 href='https://www.linkedin.com/in/ross-williams-3a433b213/'>
+                            <FaLinkedin size={20} style={{
+                                color: "#fff", marginRight: "2rem"
+                            }} />
+                            www.linkedin.com/in/ross-williams-3a433b213/
+                        </h4>
                     </div>
                 </div>
                 <div className="right">
                     <h4>
                         About Me
                     </h4>
-                    <p>This is a short description about me and what ive done</p>
-                    <div className="GitHub">
-                        <FaGithub size={20} style={{
-                            color: "#fff", marginRight: "2rem"
-                        }} />
-                    </div>
-                    <div className="Linkedin">
-                        <FaLinkedin size={20} style={{
-                            color: "#fff", marginRight: "2rem"
-                        }} />
-                    </div>
+                    <p>
+                        Im Ross,
+                        presently a Computer Science MSci student at University of Nottingham in my fourth and final year,
+                        looking for a graduate job in data or UI/UX.
+                    </p>
                 </div>
             </div>
         </div>
