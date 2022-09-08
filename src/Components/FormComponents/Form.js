e36f8c0e79461abd7f5854a2804f4b3b4e2873af
Ross <rosswilliamsproject@gmail.com>
2022-09-08 19:57:54 +0100

    v6 - implemented contact functionality

--- src/Components/FormComponents/Form.js
+++ src/Components/FormComponents/Form.js
@@ -1,31 +1,52 @@
 import "./FormStyles.css"
-import React from 'react'
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Form = () => {
+
+    const form = useRef();
+
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        emailjs.sendForm('service_zcgvpbe', 'template_cvlwx9s', form.current, 'cmACshnUpD9AHVIH2')
+            .then((result) => {
+                console.log(result.text);
+                e.target.reset();
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
     return (
         <div className="formBox">
 
-            <label className="text">
-                Your Name
-            </label>
-            <input className="box" type="text"></input>
+            <form ref={form} onSubmit={sendEmail}>
+                <label className="text">
+                    Name
+                </label>
+                <br></br>
+                <input type="text" name="user_name" className="box" />
+                <br></br>
 
-            <label className="text">
-                Email
-            </label>
-            <input className="box" type="text"></input>
+                <label className="text">
+                    Email
+                </label>
+                <br></br>
+                <input type="email" name="user_email" className="box" />
+                <br></br>
 
-            <label className="text">
-                Subject
-            </label>
-            <input className="box" type="text"></input>
+                <label className="text">
+                    Message
+                </label>
+                <br></br>
+                <textarea name="message" className="box" rows="6" placeholder="Type Your Message Here" />
+                <br></br>
 
-            <label className="text">
-                Message
-            </label>
-            <textarea className="box" rows="6" placeholder="Type Your Message Here"/>
+                <input type="submit" value="Send" className="btn" />
+            </form>
 
-            <button className="btn"> Submit</button>
         </div>
     )
 }
