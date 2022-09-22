94562e5a78efbb539e432400325728adfd228f2b
RossWilliamsProject <94560328+RossWilliamsProject@users.noreply.github.com>
2022-09-22 13:30:37 +0100

    v6.7 - improved styling project pages

--- src/Components/ProjectOverviewComponents/CovidROverview.js
+++ src/Components/ProjectOverviewComponents/CovidROverview.js
@@ -11,29 +11,32 @@ const CovidROverview = () => {
                     COVID Exploratory Data Analysis
                 </h1>
                 <p>
-                    Written in R, Blah bblah blah balha. Blah bblah blah balha. Blah bblah blah balha.
+                    Blah blah, Blah bblah blah balha. Blah bblah blah balha. Blah bblah blah balha.
                     Blah bblah blah balha.Blah bblah blah balha.Blah bblah blah balha.Blah bblah blah balha.
                     Blah bblah blah balha.Blah bblah blah balha.Blah bblah blah balha.
+                    Blah bblah blah balha.lah bblah blah balha.Blah bblah blah balha.
                     Blah bblah blah balha.
                 </p>
                 <p>
-                    "What a strange combo!" - I believe that data and UI have loads in common,
-                    both have a fundemental foundation of comprehension and understanding.
-                    In data - that understanding is recognising the story the data tells,
-                    and in UI/UX the skill is allowing users to follow their own story and interact
-                    with the data in a specific way - afterall,
-                    interface design without data is not a functional system!
+                    Blah blah, Blah bblah blah balha. Blah bblah blah balha. Blah bblah blah balha.
+                    Blah bblah blah balha.Blah bblah blah balha.Blah bblah blah balha.Blah bblah blah balha.
+                    Blah bblah blah balha.Blah bblah blah balha.Blah bblah blah balha.
+                    Blah bblah blah balha.lah bblah blah balha.Blah bblah blah balha.
+                    Blah bblah blah balha.
                 </p>
                 <p>
-                    After undertaking a few large projects using Javascript and React
-                    (such as my dissertation, and personal portfolio website),
-                    I've decided to branch out and learn more about the field of data science
-                    - with modules at university in R under my belt already, I've been brushing up on my Python skills,
-                    and learning more about machine learning.
+                    Blah blah, Blah bblah blah balha. Blah bblah blah balha. Blah bblah blah balha.
+                    Blah bblah blah balha.Blah bblah blah balha.Blah bblah blah balha.Blah bblah blah balha.
+                    Blah bblah blah balha.Blah bblah blah balha.Blah bblah blah balha.
+                    Blah bblah blah balha.lah bblah blah balha.Blah bblah blah balha.
+                    Blah bblah blah balha.
                 </p>
+                <Link to="/Projects">
+                    <button className="btn"> Back to Projects </button>
+                </Link>
             </div>
             <div className="right">
-            
+
                 <div>
                     <img src={covid} alt="covid" className="image" />
                 </div>
--- src/Components/ProjectOverviewComponents/TitanicOverview.js
+++ src/Components/ProjectOverviewComponents/TitanicOverview.js
@@ -1,43 +1,58 @@
 import "./OverviewStyles.css";
-import covid from "../../Assets/covid.jpg";
+import titanic from "../../Assets/titanic.jpg";
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const TitanicOverview = () => {
     return (
         <div className="overview">
             <div className="left">
-                <h1>
-                    TitanicOverview 
+                <h1>    
+                    TitanicOverview
                 </h1>
                 <p>
-                    Written in R, Blah bblah blah balha. Blah bblah blah balha. Blah bblah blah balha.
+                    Blah blah, Blah bblah blah balha. Blah bblah blah balha. Blah bblah blah balha.
                     Blah bblah blah balha.Blah bblah blah balha.Blah bblah blah balha.Blah bblah blah balha.
                     Blah bblah blah balha.Blah bblah blah balha.Blah bblah blah balha.
-                    Blah bblah blah balha.  
+                    Blah bblah blah balha.lah bblah blah balha.Blah bblah blah balha.
+                    Blah bblah blah balha.
                 </p>
                 <p>
-                    "What a strange combo!" - I believe that data and UI have loads in common,
-                    both have a fundemental foundation of comprehension and understanding.
-                    In data - that understanding is recognising the story the data tells,
-                    and in UI/UX the skill is allowing users to follow their own story and interact
-                    with the data in a specific way - afterall,
-                    interface design without data is not a functional system!
+                    Blah blah, Blah bblah blah balha. Blah bblah blah balha. Blah bblah blah balha.
+                    Blah bblah blah balha.Blah bblah blah balha.Blah bblah blah balha.Blah bblah blah balha.
+                    Blah bblah blah balha.Blah bblah blah balha.Blah bblah blah balha.
+                    Blah bblah blah balha.lah bblah blah balha.Blah bblah blah balha.
+                    Blah bblah blah balha.
                 </p>
                 <p>
-                    After undertaking a few large projects using Javascript and React
-                    (such as my dissertation, and personal portfolio website),
-                    I've decided to branch out and learn more about the field of data science
-                    - with modules at university in R under my belt already, I've been brushing up on my Python skills,
-                    and learning more about machine learning.
+                    Blah blah, Blah bblah blah balha. Blah bblah blah balha. Blah bblah blah balha.
+                    Blah bblah blah balha.Blah bblah blah balha.Blah bblah blah balha.Blah bblah blah balha.
+                    Blah bblah blah balha.Blah bblah blah balha.Blah bblah blah balha.
+                    Blah bblah blah balha.lah bblah blah balha.Blah bblah blah balha.
+                    Blah bblah blah balha.
                 </p>
+                <Link to="/Projects">
+                    <button className="btn"> Back to Projects </button>
+                </Link>
             </div>
             <div className="right">
+
                 <div>
-                    <img src={covid} alt="covid" className="img" />
+                    <img src={titanic} alt="titanic" className="image" />
                 </div>
+                <p>
+                    Languages Used: Blah Blah Blah Blah
+                    <br></br>
+                    Libraries Used: Blah Blah Blah Blah
+                </p>
+                <Link to="">
+                    <button className="btn"> View in GitHub </button>
+                </Link>
+
             </div>
         </div>
     )
 }
+
 
 export default TitanicOverview
